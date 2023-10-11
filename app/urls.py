6389c8b66af491318c75f31fbaaf97e1e2d40808
anhquanphamquoc <90307052+anhquanphamquoc@users.noreply.github.com>
2023-10-11 20:34:42 +0700

    #4 -dinh nghia route

---
+++ app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from app import views
+
+urlpatterns = [
+    path('',views.index,name="index"),
+    path('about',views.about,name="about"),
+    path('insert',views.insertData,name="insertData"),
+    path('update/<id>',views.updateData,name="updateData"),
+    path('delete/<id>',views.deleteData,name="deleteData"),
+]
