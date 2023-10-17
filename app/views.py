ddce1f7e56b57f395371a89d102898e60b3860ca
Nguyễn Trịnh Thành <90306525+Thanh10101@users.noreply.github.com>
2023-10-17 15:21:39 +0700

    #7 - Configurated App

--- app/views.py
+++ app/views.py
@@ -1,5 +1,24 @@
-
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from .models import Book
+from django.contrib import messages
 # Create your views here.
+def index(request):
+    data = Book.objects.all()
+    context = {"data" : data}
+    return render(request,"index.html",context)
 
+def insertData(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        desc=request.POST.get('desc')
+        date=request.POST.get('date')
+        provider=request.POST.get('provider')
+        category=request.POST.get('category')
+        print(name,desc,date,provider,category)
+        query=Book(name=name,desc=desc,date=date,provider=provider,category=category)
+        query.save()
+        # Hien thi thong bao
+        messages.info(request,"Dữ liệu sách đã được nhập vào")
+    # return render(request,"index.html")
+    return HttpResponseRedirect('/')
