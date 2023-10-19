ec0650cd0203c686a0d578f31b8b1cb7efa8ae5b
Nguyễn Trịnh Thành <90306525+Thanh10101@users.noreply.github.com>
2023-10-19 11:25:17 +0700

    #9.2 - Added Edit Delete feature

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
 from .models import Book
 from django.contrib import messages
@@ -22,3 +22,41 @@ def insertData(request):
         messages.info(request,"Dữ liệu sách đã được nhập vào")
     # return render(request,"index.html")
     return HttpResponseRedirect('/')
+
+def updateData(request,id):
+    if request.method=="POST":
+        name=request.POST['name']
+        desc=request.POST['desc']
+        date=request.POST['date']
+        provider=request.POST['provider']
+        category=request.POST['category']
+
+        edit = Book.objects.get(id=id)
+        edit.name = name
+        edit.desc = desc
+        edit.date = date
+        edit.provider = provider
+        edit.category = category
+        edit.save()
+
+        # print(name,desc,date,provider,category)
+        # query=Book(name=name,desc=desc,date=date,provider=provider,category=category)
+        # query.save()
+        messages.warning(request,"Dữ liệu sách đã được thay đổi")
+        return redirect("/")
+
+    d = Book.objects.get(id=id)
+    context = {"d" : d}
+    return render(request,"edit.html",context)
+    # return HttpResponseRedirect('/')
+
+def deleteData(request,id):
+    d = Book.objects.get(id=id)
+    d.delete()
+    # context = {"data" : data}
+    messages.error(request,"Dữ liệu sách đã được xóa")
+    return redirect("/")
+    # return HttpResponseRedirect('/')
+
+def about(request):
+    return render(request,"about.html")
